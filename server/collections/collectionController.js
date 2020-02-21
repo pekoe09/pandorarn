79dd5378f84198d121fc5504d8bcb675564ce9e1
pekoe09 <juha.kangas@welho.com>
2020-02-21 16:28:34 +0200

    Fixed returning updated version of Collection

--- server/collections/collectionController.js
+++ server/collections/collectionController.js
@@ -1,7 +1,7 @@
 const {
-	wrapAsync,
-	checkUser,
-	getMetaData,
+  wrapAsync,
+  checkUser,
+  getMetaData,
   validateMandatoryFields,
   validateUniqueness,
   findObjectById
@@ -10,59 +10,59 @@ const collectionRouter = require('express').Router()
 const Collection = require('./collection')
 
 collectionRouter.get('/', wrapAsync(async (req, res, next) => {
-	const collections = await Collection
-		.find({})
-		.sort('name')
-	res.json(collections)
+  const collections = await Collection
+    .find({})
+    .sort('name')
+  res.json(collections)
 }))
 
 collectionRouter.post('/', wrapAsync(async (req, res, next) => {
-	checkUser(req)
-	validateMandatoryFields(req, ['name'], 'collection', 'create')
+  checkUser(req)
+  validateMandatoryFields(req, ['name'], 'collection', 'create')
   await validateUniqueness(Collection, 'collection', 'name', req.body.name)
 
-	let collection = new Collection({
-		name: req.body.name,
-		description: req.body.description,
-		sets: [],
-		slots: [],
-		metaData: getMetaData(req)
-	})
-	collection = await collection.save()
+  let collection = new Collection({
+    name: req.body.name,
+    description: req.body.description,
+    sets: [],
+    slots: [],
+    metaData: getMetaData(req)
+  })
+  collection = await collection.save()
 
-	res.status(201).json(collection)
+  res.status(201).json(collection)
 }))
 
 collectionRouter.put('/:id', wrapAsync(async (req, res, next) => {
-	checkUser(req)
+  checkUser(req)
   validateMandatoryFields(req, ['name'], 'collection', 'update')
-	let collection = await findObjectById(req.params.id, Collection, 'collection')
+  let collection = await findObjectById(req.params.id, Collection, 'collection')
   await validateUniqueness(Collection, 'collection', 'name', req.body.name, collection._id)
 
-	collection.name = req.body.name
-	collection.description = req.body.description
-	collection.metaData = getMetaData(req, collection.metaData)
-	collection = await Collection.findByIdAndUpdate(collection._id, collection)
+  collection.name = req.body.name
+  collection.description = req.body.description
+  collection.metaData = getMetaData(req, collection.metaData)
+  collection = await Collection.findByIdAndUpdate(collection._id, collection, { new: true })
 
-	res.status(201).json(collection)
+  res.status(201).json(collection)
 }))
 
 collectionRouter.delete('/:id', wrapAsync(async (req, res, next) => {
-	checkUser(req)
+  checkUser(req)
   let collection = await findObjectById(req.params.id, Collection, 'collection')
 
-	if (collection.slots.length > 0) {
-		res.status(403).json({
-			error: 'Collection cannot be deleted as it still has slots.'
-		})
-	} else if (collection.sets.length > 0) {
-		res.status(403).json({
-			error: 'Collection cannot be deleted as it still has sets.'
-		})
-	} else {
-		await Collection.findByIdAndRemove(collection._id)
-		res.status(204).end()
-	}
+  if (collection.slots.length > 0) {
+    res.status(403).json({
+      error: 'Collection cannot be deleted as it still has slots.'
+    })
+  } else if (collection.sets.length > 0) {
+    res.status(403).json({
+      error: 'Collection cannot be deleted as it still has sets.'
+    })
+  } else {
+    await Collection.findByIdAndRemove(collection._id)
+    res.status(204).end()
+  }
 }))
 
 module.exports = collectionRouter
